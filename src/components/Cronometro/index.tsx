0990c79841e1ab78a21d55d2cfc0a172752ae2e1
Arcenio Souza <arcenio_neto@icloud.com>
2022-01-17 16:24:12 -0300

    Adiciona preenchimento do cronometro ao clicar no card

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,13 +1,30 @@
 import Botao from '../Botao'
 import Relogio from './Relogio'
 import style from './Cronometro.module.scss'
+import { ITarefa } from '../../types/tarefa'
+import { tempoParaSegundos } from '../../common/utils/time'
+import { useEffect, useState } from 'react'
 
-const Cronometro = () => {
+interface Props {
+   selecionado: ITarefa | undefined
+}
+
+const Cronometro = ({selecionado}: Props) => {
+
+   const [tempo, setTempo] = useState<number>()
+
+   useEffect(() => {
+      if(selecionado?.tempo){
+         setTempo(tempoParaSegundos(selecionado.tempo))
+      }
+   }, [selecionado])
+   
+
    return (
       <div className={style.cronometro}>
          <p className={style.titulo}>Escolha um card e inicie o Cronômetro</p>
          <div className={style.relogioWrapper}>
-         <Relogio />
+         <Relogio tempo={tempo}/>
          </div>
          <Botao texto="Começar" />
       </div>
