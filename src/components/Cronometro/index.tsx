71ec858087937b1c2fe21ca4d6ae445217e69a0a
Arcenio Souza <arcenio_neto@icloud.com>
2022-01-17 09:25:46 -0300

    Adiciona cronometro ao projeto

---
+++ src/components/Cronometro/index.tsx
@@ -0,0 +1,17 @@
+import Botao from '../Botao'
+import Relogio from './Relogio'
+import style from './Cronometro.module.scss'
+
+const Cronometro = () => {
+   return (
+      <div className={style.cronometro}>
+         <p className={style.titulo}>Escolha um card e inicie o Cronômetro</p>
+         <div className={style.relogioWrapper}>
+         <Relogio />
+         </div>
+         <Botao texto="Começar" />
+      </div>
+   )
+}
+
+export default Cronometro
