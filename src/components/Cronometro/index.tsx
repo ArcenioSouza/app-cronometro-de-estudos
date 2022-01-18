db7f4dc33b4db8f841c0e5ef57ca17dae6534096
Arcenio Souza <arcenio_neto@icloud.com>
2022-01-18 09:29:08 -0300

    Adiciona função para fazer o cronometro regredir

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -18,6 +18,15 @@ const Cronometro = ({selecionado}: Props) => {
          setTempo(tempoParaSegundos(selecionado.tempo))
       }
    }, [selecionado])
+
+   function regressiva(contador: number = 0){
+      setTimeout(() => {
+         if(contador > 0){
+            setTempo(contador - 1)
+            return regressiva(contador - 1)
+         }
+      }, 1000);
+   }
    
 
    return (
@@ -26,7 +35,10 @@ const Cronometro = ({selecionado}: Props) => {
          <div className={style.relogioWrapper}>
          <Relogio tempo={tempo}/>
          </div>
-         <Botao texto="Começar" />
+         <Botao 
+            texto="Começar" 
+            onClick={() => regressiva(tempo)} 
+         />
       </div>
    )
 }
